38d4cd8ea927e55f27341a3c049e3274b83ba14c
BVRIT <BVRIT@ECE-LAB-198>
2020-02-17 15:44:13 +0530

    demos

---
+++ movie/src/main/java/com/capg/movie/Demo.java
@@ -0,0 +1,10 @@
+package com.capg.movie;
+
+public class Demo {
+
+	public static int  method1(int x,int y)
+	{
+	return x+y;	
+	}
+
+}
---
+++ movie/src/test/java/com/capg/movie/DemoTest.java
@@ -0,0 +1,16 @@
+package com.capg.movie;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class DemoTest extends TestCase {
+
+	@Test
+	public void testMethod1() {
+		
+
+assertEquals(12, Demo.method1(6, 6));
+	}
+
+}
